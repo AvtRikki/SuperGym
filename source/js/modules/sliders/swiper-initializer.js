96a42e9484f9dc6ae8f9b3b35e3b8d63be9b69ca
bogza.anton <bogza.anton@gmail.com>
2024-07-21 10:25:28 -0700

    Fixed logic of scrolling on tab control

--- source/js/modules/sliders/swiper-initializer.js
+++ source/js/modules/sliders/swiper-initializer.js
@@ -5,17 +5,48 @@ export class SwiperInitializer {
 
   constructor(slideClassName) {
     this.slideClassName = slideClassName;
+    this.swiper = null;
+  }
+
+  #DisableLoopState() {
+    if (this.swiper.params.loop) {
+      this.swiper.params.loop = false;
+      this.swiper.update();
+    }
+  }
+
+  #EnableLoopState() {
+    if (!this.swiper.params.loop) {
+      this.swiper.params.loop = true;
+      this.swiper.update();
+    }
   }
 
   Initialize(options) {
-    const slides = document.querySelectorAll(`.${this.#SWIPER_DATA_SLIDE}`);
-    slides.forEach((slide) => {
-      slide.setAttribute('tabindex', '0');
-    });
-
     const slider = document.querySelector(`.${this.slideClassName}`);
     if (slider) {
-      return new Swiper(slider, options);
+      this.swiper = new Swiper(slider, options);
+      const slides = document.querySelectorAll(`.${this.#SWIPER_DATA_SLIDE}`);
+      slides.forEach((slide) => {
+        slide.setAttribute('tabindex', '0');
+      });
+
+      document.addEventListener('keydown', (event) => {
+        if (event.key === 'Tab') {
+          this.#DisableLoopState();
+        }
+      });
+
+      document.addEventListener('keyup', (event) => {
+        if (event.key === 'Tab') {
+          this.#EnableLoopState();
+        }
+      });
+
+      document.querySelector(options.navigation.nextEl).addEventListener('click', this.#EnableLoopState);
+      document.querySelector(options.navigation.prevEl).addEventListener('click', this.#EnableLoopState);
+
+      return this.swiper;
     }
   }
 
@@ -25,7 +56,7 @@ export class SwiperInitializer {
         nextEl: `.${nextSlideClassName}`,
         prevEl: `.${prevSlideClassName}`,
       },
-      slidesPerView: 8,
+      slidesPerView: 4,
       loop: true,
       spaceBetween: 40,
       breakpoints: {
